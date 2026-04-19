479e85ae2aa1ddf18858de2ba6e724ce6f4bc065
tobinguyenx <nguyentaitino@gmail.com>
2026-04-19 14:42:07 +1000

    toogle showhide list user

--- src/components/DisplayInfor.js
+++ src/components/DisplayInfor.js
@@ -1,18 +1,46 @@
 import React from "react";
 
 class DisplayInfor extends React.Component {
+  handleShowHide = () => {
+    this.setState({
+      isShowListUser: !this.state.isShowListUser,
+    });
+  };
+  state = {
+    isShowListUser: true,
+  };
   render() {
     console.log(this.props);
     const { listUsers } = this.props;
+
     return (
       <div>
         This is DisplayInfor
         <div>
           My name is Tai Nguyen DisplayInfor
+          <br />
+          <br />
+          <hr />
           {listUsers.map((user) => {
             return (
-              <div key={user.id} className={+user.age > 18 ? "green" : "red"}>
-                My name is {user.name}
+              <div>
+                <div>
+                  <span
+                    onClick={() => {
+                      this.handleShowHide();
+                    }}
+                  >
+                    Hide List User
+                  </span>
+                </div>
+                {this.state.isShowListUser && (
+                  <div
+                    key={user.id}
+                    className={+user.age > 18 ? "green" : "red"}
+                  >
+                    My name is {user.name}
+                  </div>
+                )}
               </div>
             );
           })}
