3dc04a88a76312570a73af039a3835f80baa635c
tobinguyenx <nguyentaitino@gmail.com>
2026-04-19 23:38:52 +1000

    useState React Hook

--- src/components/DisplayInfor.js
+++ src/components/DisplayInfor.js
@@ -1,50 +1,33 @@
-import React from "react";
+import React, { useState } from "react";
 
-class DisplayInfor extends React.Component {
-  handleShowHide = () => {
-    this.setState({
-      isShowListUser: !this.state.isShowListUser,
-    });
+const DisplayInfor = (props) => {
+  const [listUsers, handleDeleteUser] = props;
+
+  const [isShowListUser, setIsShowListUser] = useState();
+
+  const handleShowHide = () => {
+    setIsShowListUser(!isShowListUser);
   };
-  state = {
-    isShowListUser: true,
-  };
 
-  render() {
-    const { listUsers } = this.props;
-
-    return (
+  return (
+    <div>
+      This is DisplayInfor Screen
       <div>
-        This is DisplayInfor Screen
-        <div>
-          <button
-            onClick={() => {
-              this.handleShowHide();
-            }}
-          >
-            {this.state.isShowListUser === true ? "Hide" : "Show"}
-          </button>
+        <button onClick={handleShowHide}>
+          {isShowListUser ? "Hide" : "Show"}
+        </button>
 
-          {this.state.isShowListUser &&
-            listUsers.map((user) => {
-              return (
-                <div key={user.id} className={+user.age > 18 ? "green" : "red"}>
-                  My name is {user.name}
-                  <br />
-                  <button
-                    onClick={() => {
-                      this.props.handleDeleteUser(user.id);
-                    }}
-                  >
-                    Delete
-                  </button>
-                </div>
-              );
-            })}
-        </div>
+        {isShowListUser &&
+          listUsers.map((user) => (
+            <div key={user.id} className={user.age > 18 ? "green" : "red"}>
+              My name is {user.name}
+              <br />
+              <button onClick={() => handleDeleteUser(user.id)}>Delete</button>
+            </div>
+          ))}
       </div>
-    );
-  }
-}
+    </div>
+  );
+};
 
 export default DisplayInfor;
