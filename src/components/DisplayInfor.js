3f3d1ba6762763684d928715714ed1d8aa747744
tobinguyenx <nguyentaitino@gmail.com>
2026-04-19 16:14:28 +1000

    button showhide list user

--- src/components/DisplayInfor.js
+++ src/components/DisplayInfor.js
@@ -9,41 +9,29 @@ class DisplayInfor extends React.Component {
   state = {
     isShowListUser: true,
   };
+
   render() {
-    console.log(this.props);
     const { listUsers } = this.props;
 
     return (
       <div>
-        This is DisplayInfor
+        This is DisplayInfor Screen
         <div>
-          My name is Tai Nguyen DisplayInfor
-          <br />
-          <br />
-          <hr />
-          {listUsers.map((user) => {
-            return (
-              <div>
-                <div>
-                  <span
-                    onClick={() => {
-                      this.handleShowHide();
-                    }}
-                  >
-                    Hide List User
-                  </span>
+          <button
+            onClick={() => {
+              this.handleShowHide();
+            }}
+          >
+            Hide
+          </button>
+          {this.state.isShowListUser &&
+            listUsers.map((user) => {
+              return (
+                <div key={user.id} className={+user.age > 18 ? "green" : "red"}>
+                  My name is {user.name}
                 </div>
-                {this.state.isShowListUser && (
-                  <div
-                    key={user.id}
-                    className={+user.age > 18 ? "green" : "red"}
-                  >
-                    My name is {user.name}
-                  </div>
-                )}
-              </div>
-            );
-          })}
+              );
+            })}
         </div>
       </div>
     );
